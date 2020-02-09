3df7535642bfb0aa5ff8a76fc946050a23c240db
wizardywizard <littledavid175@gmail.com>
2020-02-09 12:23:13 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+    let des = document.getElementById("new-task-description").value
+    
+  document.getElementById("submit").addEventListener("click", (event) => {
+    event.preventDefault();
+    let list = document.createElement("li")
+    let tasks = document.getElementById("tasks")
+    let des = document.getElementById("new-task-description").value    
+    list.innerText = des    
+    tasks.append(list)     
+  })  
 });
